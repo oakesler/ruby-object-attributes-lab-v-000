b51041d26a041d796ca8ab28e4381210fae08d3e
Olivia Kesler <oakesler@protonmail.ch>
2019-07-31 22:36:57 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person 
+  def name= (persons_name)
+    @name = persons_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(persons_job)
+    @breed = persons_job
+  end
+  
+  def breed
+    @breed
+  end
+end
